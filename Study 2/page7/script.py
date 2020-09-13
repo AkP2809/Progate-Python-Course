61161d23eacb51f6b30e271d386666679fe9e1f2
Armaan Khan <akp.wifi@gmail.com>
2020-09-13 22:27:59 +0530

    Too Lazy to write msg

---
+++ Study 2/page7/script.py
@@ -0,0 +1,9 @@
+fruits = {'apple': 'manzana', 'orange': 'naranja', 'grape': 'uva'}
+
+# With a for loop, print '___ is ___ in Spanish'
+'''
+for k,v in fruits.items:
+    print("{} is {} in Spanish".format(k,v))
+'''
+for i in fruits:
+    print("{} is {} in Spanish".format(i,fruits[i]))
